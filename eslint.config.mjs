a497744bbf85f92fb3cf93dbc66c730ca604d314
Tanay <singhaniatanay18@gmail.com>
2025-05-11 16:56:19 +0530

    root package rule config

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier"; // <-- Add this import
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier, // <-- Add this to the array
+]);
